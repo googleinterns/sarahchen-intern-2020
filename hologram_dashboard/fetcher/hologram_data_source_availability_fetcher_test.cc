0f1ea918803e0ba190f51aad19d4002a4aa72f27
Alex <alexanderlin@google.com>
2020-06-13 01:05:51 +0000

    changed test case for better style

--- hologram_dashboard/fetcher/hologram_data_source_availability_fetcher_test.cc
+++ hologram_dashboard/fetcher/hologram_data_source_availability_fetcher_test.cc
@@ -15,35 +15,29 @@
 */
 
 #include "hologram_data_source_availability_fetcher.h"
-#include <filesystem>
+#include <google/protobuf/util/message_differencer.h>
 
 namespace wireless_android_play_analytics{
 
 TEST(FetcherTest, AcquireConfigTest) {
     // Test malformed/invalid path.
-    HologramDataSourceAvailabilityFetcher h;
-    ASSERT_DEATH(h.AcquireConfig("/abc"), "");
-    ASSERT_DEATH(
-        h.AcquireConfig("fetcher/hologram_config_malformed.config"), "");
+    HologramDataSourceAvailabilityFetcher hologram_fetcher;
+    ASSERT_DEATH(hologram_fetcher.AcquireConfig("/abc"), "");
+    ASSERT_DEATH(hologram_fetcher.AcquireConfig(
+        "fetcher/testdata/hologram_config_malformed.config"), "");
 
     // Test correct path.
-    h.AcquireConfig("fetcher/hologram_config.config");
-    std::ifstream ins("fetcher/hologram_config.config");
-    std::string compare;
-    while(ins.good()) {
-        std::string tmp;
-        ins >> tmp;
-        compare += tmp;
-    }
-    std::string hologram_config_debug = h.hologram_configs_.DebugString();
-    // Remove all spaces and endline characters.
-    hologram_config_debug.erase(
-        remove(hologram_config_debug.begin(), hologram_config_debug.end(), ' '), 
-            hologram_config_debug.end());
-    hologram_config_debug.erase(
-        remove(hologram_config_debug.begin(), hologram_config_debug.end(), '\n'), 
-            hologram_config_debug.end());
-    ASSERT_EQ(hologram_config_debug, compare);
+    hologram_fetcher.AcquireConfig("fetcher/testdata/hologram_config_test.config");
+    HologramConfigSet expected_hologram_config;
+    // Create a proto with expected values
+    HologramConfig* config = expected_hologram_config.add_data_source_config();
+    config->set_source_type(DataSource::APP_TYPE_SOURCE);
+    config->set_kvick_corpus(Corpus::HOLOGRAM_DEFAULT);
+    config = expected_hologram_config.add_data_source_config();
+    config->set_source_type(DataSource::USER_ATTRIBUTE);
+    config->set_kvick_corpus(Corpus::HOLOGRAM_USER_ATTRIBUTE);
+    
+    ASSERT_TRUE(google::protobuf::util::MessageDifferencer::Equals(expected_hologram_config, hologram_fetcher.hologram_configs_));
 }
 
 } // namespace wireless_android_play_analytics
