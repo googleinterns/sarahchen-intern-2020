dddd87759d9a0a62766b2eef4ed64e64bf6d0772
Alex <alexanderlin@google.com>
2020-07-15 21:49:29 +0000

    stuck on testing unique_ptrs and protovalue pointers

--- proto_parser/src/message_value_test.cc
+++ proto_parser/src/message_value_test.cc
@@ -16,26 +16,50 @@
 
 #include "message_value.h"
 #include "gmock/gmock.h"
-#include "proto/event_predicate.pb.h"
 
 namespace wireless_android_play_analytics{
  
-class PrimitiveValueTest : public ::testing::Test {
+class MessageValueTest : public ::testing::Test {
  protected:
-  PrimitiveValueTest()
+  MessageValueTest()
     : message_(MessageValue("test", 0, 0)) { }
 
-  virtual void SetUp() { }
+  virtual void SetUp() { 
+    field_0_ = absl::make_unique<MessageValue>("temp", 0, 0);
+    field_1_ = absl::make_unique<MessageValue>("temp1", 0, 1);
+    field_2_ = absl::make_unique<MessageValue>("temp2", 0, 2);
+    ProtoValue* field_0_ptr_ = field_0_.get();
+    ProtoValue* field_1_ptr_ = field_1_.get();
+    ProtoValue* field_2_ptr_ = field_2_.get();
+  }
 
   MessageValue message_;
+  std::unique_ptr<ProtoValue> field_0_;
+  std::unique_ptr<ProtoValue> field_1_;
+  std::unique_ptr<ProtoValue> field_2_;
+  ProtoValue* field_0_ptr_;
+  ProtoValue* field_1_ptr_;
+  ProtoValue* field_2_ptr_;
 };
 
-TEST_F(PrimitiveValueTest, ProtoValueGetterTests) {
+TEST_F(MessageValueTest, ProtoValueGetterTests) {
   EXPECT_EQ(message_.GetName(), "test");
   EXPECT_EQ(message_.GetCommentAboveField().size(), 0);
   EXPECT_EQ(message_.GetCommentBehindField(), "");
   EXPECT_EQ(message_.GetLineNumber(), 0);
   EXPECT_EQ(message_.GetIndentCount(), 0);
+}
+
+TEST_F(MessageValueTest, AddFieldsTest) {
+  message_.AddField(std::move(field_2_));
+  message_.AddField(std::move(field_0_)); 
+  message_.AddField(std::move(field_1_)); 
+  EXPECT_THAT(message_.GetFieldsMutable(), 
+      testing::ElementsAreArray({field_2_ptr_, field_0_ptr_, field_1_ptr_}));
+}
+
+TEST_F(MessageValueTest, SortFieldsTest) {
+  message_.SortFields();
 }
 
 } // namespace wireless_android_play_analytics
