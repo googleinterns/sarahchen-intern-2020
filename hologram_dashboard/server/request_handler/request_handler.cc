e8e18e279d1af76bee9949fbaa3c4720ca87ad93
Alex <alexanderlin@google.com>
2020-07-16 23:51:44 +0000

    changed what the server sends

--- hologram_dashboard/server/request_handler/request_handler.cc
+++ hologram_dashboard/server/request_handler/request_handler.cc
@@ -38,34 +38,7 @@ void RequestHandler::GetDashboard(const Pistache::Rest::Request& request,
         "5/15/2020", "5/14/2020", "5/13/2020"};
     std::string system_in = request.param(":system").as<std::string>();
     std::string status = "111010111110001011100100111000100110001010000010100111010";
-    auto stream = response.stream(Pistache::Http::Code::Ok);
-    const char* system = system_in.c_str();
-    // track the online or offline bits
-    int i = 0;
-
-    // Since the html file may be large, stream the data as we write it for
-    // more efficiency
-    stream << "<table id=\"" << system << "%TEXT\">\n";
-    stream << "\t<br/>\n\t<p>" << system << "</p>\n";
-
-    for(const std::string &data : dataSources) {
-        stream << "\t<tr>\n";
-        stream << "\t\t<td>" << data.c_str() << "&nbsp&nbsp</td>\n";
-        for(const std::string& date : dates) {
-            stream << "\t\t<td class=";
-            if (status[i++] == '1') {
-                stream << "\"online\"";
-            }
-            else {
-                stream << "\"offline\"";
-            }
-            stream << " title=\"" << date.c_str() << "\" id=\"" << data.c_str() 
-                << "%" << date.c_str() << "%" << system << "\"></td>\n";
-        }
-        stream << "\t</tr>\n\n";
-    }
-    stream << "</table>\n";
-    stream.ends();
+    response.send(Pistache::Http::Code::Ok, "{\"message\": \"Hello World\"}");
 }
 
 void RequestHandler::PopulateBackfillCommand
