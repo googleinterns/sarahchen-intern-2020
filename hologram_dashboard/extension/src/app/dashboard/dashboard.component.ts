3f797edbd8997c3088106f3777a4ca5a54ed2282
Alex <alexanderlin@google.com>
2020-07-21 22:03:48 +0000

    finished front end code just need to modify manifest.json

--- hologram_dashboard/extension/src/app/dashboard/dashboard.component.ts
+++ hologram_dashboard/extension/src/app/dashboard/dashboard.component.ts
@@ -1,20 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { RequestHandler } from '../request-handler.component'
 
-@Injectable()
-export class RequestHandler {
-  url: string
-  constructor(private http: HttpClient) { 
-    this.url = "http://localhost:4200/"
-  }
+export interface DateAvailability {
+  date: string;
+  status: boolean;
+}
 
-  getDashboard(system: string) {
-    let html = this.url.concat(system);
-    return this.http.get(html);
-  }
+export interface HologramDataAvailability {
+  name: string;
+  dates: Array<DateAvailability>
 }
 
 @Component({
@@ -24,21 +18,57 @@ export class RequestHandler {
   providers: [ RequestHandler ]
 })
 export class DashboardComponent implements OnInit {
-  chipper_dashboard: string;
-  chipper_gdpr_dashboard: string;
+  dashboard: Array<HologramDataAvailability>;
+  chipper: boolean
+  chipper_gdpr: boolean
+  display: boolean
 
   constructor(private requestHandler: RequestHandler) { 
-    this.chipper_dashboard = "";
-    this.chipper_gdpr_dashboard = "";
+    this.chipper = false;
+    this.chipper_gdpr = false;
+    this.display = false;
+    this.dashboard = []
   }
 
   ngOnInit(): void {
   }
 
-  onClick(system: string) {
-    this.requestHandler.getDashboard(system).toPromise()
-      .then((data) => {this.chipper_dashboard = data["message"]})
-      .then(() => {console.log(this.chipper_dashboard)});
+  renderTemplate(hologram_data_availability: JSON, system: string) {
+    for (let key in hologram_data_availability) {
+      let data_set_availability = {} as HologramDataAvailability;
+      data_set_availability.name = key;
+      data_set_availability.dates = [];
+      hologram_data_availability[key].forEach(data => {
+        let date_info = {} as DateAvailability;
+        date_info.date = data[0];
+        date_info.status = data[1];
+        data_set_availability.dates.push(date_info);
+      });
+      this.dashboard.push(data_set_availability);
+    }
   }
 
+  onClick(system: string) {
+    if (system === "Chipper") {
+      if (this.chipper) {
+        this.chipper = false;
+      } else {
+        this.chipper = true;
+        this.chipper_gdpr = false;
+      }
+    } else if (system === "Chipper_GDPR") {
+      if (this.chipper_gdpr) {
+        this.chipper_gdpr = false;
+      } else {
+        this.chipper_gdpr = true;
+        this.chipper = false;
+      }
+    }
+    this.dashboard = [];
+    this.display = (this.chipper || this.chipper_gdpr);
+    if(this.display) {
+      this.requestHandler.getDashboard(system).toPromise()
+          .then((data) => this.renderTemplate(data, system));
+    }
+  }
 }
