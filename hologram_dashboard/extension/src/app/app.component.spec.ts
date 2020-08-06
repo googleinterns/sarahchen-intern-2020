c84ed98c860a0088ed5645b421c479a4ef8bb4f9
Alex <alexanderlin@google.com>
2020-08-06 00:27:46 +0000

    removed different test case

--- hologram_dashboard/extension/src/app/app.component.spec.ts
+++ hologram_dashboard/extension/src/app/app.component.spec.ts
@@ -14,15 +14,15 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
   it(`should have as title 'extension'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('extension');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
   });
 });
